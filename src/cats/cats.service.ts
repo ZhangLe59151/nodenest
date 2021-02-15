220ddebb947abe8cf55a32bbde2d9561d9679187
zhangle <zhanglenus@gmail.com>
2021-02-15 09:37:18 +0800

    update structure

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -4,17 +4,19 @@ import * as fs from 'fs';
 
 @Injectable()
 export class CatsService {
-  private readonly cats: Cat[] = JSON.parse(fs.readFileSync('data/cats.json').toString());
+  private readonly cats: Cat[] = JSON.parse(
+    fs.readFileSync('src/data/cats.json').toString(),
+  );
 
   create(cat: Cat) {
     // this.cats.push(cat);
-    let a = JSON.parse(fs.readFileSync('data/cats.json').toString())
+    const a = JSON.parse(fs.readFileSync('src/data/cats.json').toString());
     a.push(cat);
     console.info('cats: ', a);
-    fs.writeFileSync('data/cats.json', JSON.stringify(a));
+    fs.writeFileSync('src/data/cats.json', JSON.stringify(a));
   }
 
   async findAll(): Promise<any> {
-    return JSON.parse(fs.readFileSync('data/cats.json').toString()); 
+    return JSON.parse(fs.readFileSync('src/data/cats.json').toString());
   }
 }
